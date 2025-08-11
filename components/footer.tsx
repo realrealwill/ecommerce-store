4fcde258a027b672b4e287e6a1ca996355fbf026
realrealwill <166269969+realrealwill@users.noreply.github.com>
2025-08-11 04:24:53 -0500

    final

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-autp py-10">
+        <p className="text-center text-xs text-black">
+          &copy, 2025 StoreNameA, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
+export default Footer
