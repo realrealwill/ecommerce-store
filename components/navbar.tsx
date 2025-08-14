9d572f1be8fc1ebf9a647b227024f2423dc6304f
realrealwill <166269969+realrealwill@users.noreply.github.com>
2025-08-13 23:05:02 -0500

    test deployment category issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,11 +8,11 @@ export const revalidate = 0;
 
 const Navbar = async () => {
   const categories = await getCategories();
-  // console.log(categories);
+  console.log(categories);
   return (
     <div className="border-b">
       <Container>
-        <div className="relative px-4 sm: px-6 lg: px-8 flex h-16 items-center">
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
